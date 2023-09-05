635e92457d9deaf225d90e86170e7e7de46e4979
nguyenducde <ducde678@gmail.com>
2023-09-05 14:06:45 +0700

    first

--- server/middlewares/10_cors.js
+++ server/middlewares/10_cors.js
@@ -1,7 +1,11 @@
 import cors from 'cors';
 
 export default cors({
-  origin: ["https://shopauthenticdalat.com", "http://localhost:3000","http://localhost:3000"],
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:3000",
+    "https://shopauthenticdalat.com",
+  ],
   methods: ["GET", "PUT", "POST", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
   credentials: true,
